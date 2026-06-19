a6475b8b43ad297d3c9310f09f9f28c2ad92369c
Emmanual <127601685+EmmanualJanuarie@users.noreply.github.com>
2026-06-19 16:54:35 +0200

    new code

--- script.js
+++ script.js
@@ -1,79 +1,365 @@
 const workouts = {
 
 Monday: [
-{
-name: "Bicep Curls",
-sets: "4",
-reps: "12",
-instructions:
-"Keep your elbows close to your body. Lift with control and lower slowly."
-},
-{
-name: "Push Ups",
-sets: "4",
-reps: "15",
-instructions:
-"Keep your body straight and lower your chest fully."
-}
+    {
+        name:"BB Bench Press",
+        sets:"5",
+        reps:"5-10",
+        rest:"2 mins",
+        tempo:"3 In - 1 Hold - 2 Push",
+        instructions:[
+            "Pull Shoulder blades back into bench",
+            "Chest up",
+            "Lower bar to chest",
+            "Drive Up Hard",
+            "Final set TO FAILURE"
+        ]
+    },
+    {
+        name:"Barbell Row",
+        sets:"5",
+        reps:"8-12",
+        rest:"2 mins",
+        tempo:"3 In - 1 Hold - 2 Push",
+        instructions:[
+            "Hinge hips",
+            "Back Flat",
+            "Pull bar towards upper stomach",
+            "Squeeze back for 1s"
+        ]
+    },
+    {
+        name:"Pull-Up Progression",
+        sets:"5",
+        reps:"10",
+        rest:"2 mins",
+        tempo:"Lower for 5 - 8s",
+        instructions:[
+            "Jump to top of bar",
+            "Lower slowly for 5 - 8s",
+            "Pull elbows towards hips",
+            "Final set to failure"
+        ]
+    },
+    {
+        name:"Chair Dips",
+        sets:"4",
+        reps:"15",
+        rest:"1 min",
+        tempo:"3 In - 1 Hold - 2 Push",
+        instructions:[
+            "Chest Up",
+            "Lower under control",
+            "Drive back up hard",
+            "Final set to failure"
+        ]
+    },
+    {
+        name:"Barbell Curls",
+        sets:"4",
+        reps:"10 - 15",
+        rest:"1 min",
+        tempo:"3 In - 1 Hold - 2 Push",
+        instructions:[
+            "Elbows fixed at sides",
+            "Curl all the way up",
+            "Squeeze for 1s",
+            "Lower for 3s",
+            "Last set to failure"
+        ]
+    },
+    {
+        name:"Overhead Press",
+        sets:"4",
+        reps:"6 - 12",
+        rest:"2 mins",
+        tempo:"3 In - 1 Hold - 2 Push",
+        instructions:[
+            "Squeeze glutes and abs",
+            "Press in a striaght line",
+            "Lower under control"
+        ]
+    }
 ],
 
 Tuesday: [
-{
-name: "Squats",
-sets: "4",
-reps: "20",
-instructions:
-"Push your hips back and keep your knees aligned with your feet."
-},
-{
-name: "Lunges",
-sets: "3",
-reps: "12",
-instructions:
-"Step forward and lower until both knees form 90 degree angles."
-}
+    {
+        name:"BB Back Squats",
+        sets:"5",
+        reps:"5 - 10",
+        rest:"2 mins",
+        tempo:"3 In - 1 Hold - 2 Push",
+        instructions:[
+            "Feet roughly shoulder width",
+            "Brace your core before each rep",
+            "Go deep as possible",
+            "Keep back neutral",
+            "Drive up agressively"
+        ]
+    },
+    {
+        name:"Romanian Deadlifts",
+        sets:"5",
+        reps:"8 - 12",
+        rest:"2 mins",
+        tempo:"3 In - 1 Hold - 2 Push",
+        instructions:[
+            "Push hips back",
+            "Slight bend in knees",
+            "Keep bar close to legs",
+            "Feel stretch in hamstrings",
+            "Squeeze glutes at top"
+        ]
+    },
+    {
+        name:"DB Walking Lunges",
+        sets:"5",
+        reps:"12 Steps/Leg",
+        rest:"1 min",
+        tempo:"no tempo",
+        instructions:[
+            "Take long steps",
+            "Lower under control",
+            "Push through the front foot"
+        ]
+    },
+    {
+        name:"BB Bulgarian Squat",
+        sets:"3",
+        reps:"10 - 15",
+        rest:"2 mins",
+        tempo:"3 In - 1 Hold - 2 Push",
+        instructions:[
+            "Rear foot elevated",
+            "Slow decend",
+            "Drive up through front leg"
+        ]
+    },
+    {
+        name:"Stand. Calf Raises",
+        sets:"5",
+        reps:"20",
+        rest:"1 min",
+        tempo:"3 In - 1 Hold - 2 Push",
+        instructions:[
+            "Full Stretch at bottom",
+            "Pause 1s",
+            "Rise as high as possible",
+            "Squeeze 1s at top",
+            "last set to failure"
+        ]
+    },
+    {
+        name:"Seated Calf Raises",
+        sets:"5",
+        reps:"20",
+        rest:"1 min",
+        tempo:"3 In - 1 Hold - 2 Push",
+        instructions:[
+            "Sit upright, knees bent 90d",
+            "Place plate on lap",
+            "Place balls of feet on edge of plate(floor plate)",
+            "Lower heels fully to get deep stretch",
+            "Pause for 2s at the top, then lower slowly"
+        ]
+    },
+    {
+        name:"Ab Roller",
+        sets:"5",
+        reps:"10 - 15",
+        rest:"1 min",
+        tempo:"3 out - 1 Hold - 2 In",
+        instructions:[
+            "Glutes tight",
+            "Don't sag lower back",
+            "Roll out only as far as you can",
+            "Last set to failure"
+        ]
+    }
 ],
 
 Thursday: [
-{
-name: "Pull Ups",
-sets: "4",
-reps: "8",
-instructions:
-"Pull your chest toward the bar and control the descent."
-},
-{
-name: "Rows",
-sets: "4",
-reps: "12",
-instructions:
-"Squeeze your shoulder blades together on every rep."
-}
+    {
+        name:"BB Bench Press",
+        sets:"5",
+        reps:"5-10",
+        rest:"2 mins",
+        tempo:"3 In - 1 Hold - 2 Push",
+        instructions:[
+            "Pull Shoulder blades back into bench",
+            "Chest up",
+            "Lower bar to chest",
+            "Drive Up Hard",
+            "Final set TO FAILURE"
+        ]
+    },
+    {
+        name:"Barbell Row",
+        sets:"5",
+        reps:"8-12",
+        rest:"2 mins",
+        tempo:"3 In - 1 Hold - 2 Push",
+        instructions:[
+            "Hinge hips",
+            "Back Flat",
+            "Pull bar towards upper stomach",
+            "Squeeze back for 1s"
+        ]
+    },
+    {
+        name:"Pull-Up Progression",
+        sets:"5",
+        reps:"10",
+        rest:"2 mins",
+        tempo:"Lower for 5 - 8s",
+        instructions:[
+            "Jump to top of bar",
+            "Lower slowly for 5 - 8s",
+            "Pull elbows towards hips",
+            "Final set to failure"
+        ]
+    },
+    {
+        name:"Chair Dips",
+        sets:"4",
+        reps:"15",
+        rest:"1 min",
+        tempo:"3 In - 1 Hold - 2 Push",
+        instructions:[
+            "Chest Up",
+            "Lower under control",
+            "Drive back up hard",
+            "Final set to failure"
+        ]
+    },
+    {
+        name:"Barbell Curls",
+        sets:"4",
+        reps:"10 - 15",
+        rest:"1 min",
+        tempo:"3 In - 1 Hold - 2 Push",
+        instructions:[
+            "Elbows fixed at sides",
+            "Curl all the way up",
+            "Squeeze for 1s",
+            "Lower for 3s",
+            "Last set to failure"
+        ]
+    },
+    {
+        name:"Overhead Press",
+        sets:"4",
+        reps:"6 - 12",
+        rest:"2 mins",
+        tempo:"3 In - 1 Hold - 2 Push",
+        instructions:[
+            "Squeeze glutes and abs",
+            "Press in a striaght line",
+            "Lower under control"
+        ]
+    }
 ],
 
 Friday: [
-{
-name: "Shoulder Press",
-sets: "4",
-reps: "10",
-instructions:
-"Press overhead through a full range of motion."
-},
-{
-name: "Lateral Raises",
-sets: "3",
-reps: "15",
-instructions:
-"Lift arms to shoulder height while keeping control."
-}
-]
+    {
+        name:"BB Back Squats",
+        sets:"5",
+        reps:"5 - 10",
+        rest:"2 mins",
+        tempo:"3 In - 1 Hold - 2 Push",
+        instructions:[
+            "Feet roughly shoulder width",
+            "Brace your core before each rep",
+            "Go deep as possible",
+            "Keep back neutral",
+            "Drive up agressively"
+        ]
+    },
+    {
+        name:"Romanian Deadlifts",
+        sets:"5",
+        reps:"8 - 12",
+        rest:"2 mins",
+        tempo:"3 In - 1 Hold - 2 Push",
+        instructions:[
+            "Push hips back",
+            "Slight bend in knees",
+            "Keep bar close to legs",
+            "Feel stretch in hamstrings",
+            "Squeeze glutes at top"
+        ]
+    },
+    {
+        name:"DB Walking Lunges",
+        sets:"5",
+        reps:"12 Steps/Leg",
+        rest:"1 min",
+        tempo:"no tempo",
+        instructions:[
+            "Take long steps",
+            "Lower under control",
+            "Push through the front foot"
+        ]
+    },
+    {
+        name:"BB Bulgarian Squat",
+        sets:"3",
+        reps:"10 - 15",
+        rest:"2 mins",
+        tempo:"3 In - 1 Hold - 2 Push",
+        instructions:[
+            "Rear foot elevated",
+            "Slow decend",
+            "Drive up through front leg"
+        ]
+    },
+    {
+        name:"Stand. Calf Raises",
+        sets:"5",
+        reps:"20",
+        rest:"1 min",
+        tempo:"3 In - 1 Hold - 2 Push",
+        instructions:[
+            "Full Stretch at bottom",
+            "Pause 1s",
+            "Rise as high as possible",
+            "Squeeze 1s at top",
+            "last set to failure"
+        ]
+    },
+    {
+        name:"Seated Calf Raises",
+        sets:"5",
+        reps:"20",
+        rest:"1 min",
+        tempo:"3 In - 1 Hold - 2 Push",
+        instructions:[
+            "Sit upright, knees bent 90d",
+            "Place plate on lap",
+            "Place balls of feet on edge of plate(floor plate)",
+            "Lower heels fully to get deep stretch",
+            "Pause for 2s at the top, then lower slowly"
+        ]
+    },
+    {
+        name:"Ab Roller",
+        sets:"5",
+        reps:"10 - 15",
+        rest:"1 min",
+        tempo:"3 out - 1 Hold - 2 In",
+        instructions:[
+            "Glutes tight",
+            "Don't sag lower back",
+            "Roll out only as far as you can",
+            "Last set to failure"
+        ]
+    }
+],
 
 };
 
-const today = new Date().toLocaleDateString(
-"en-US",
-{ weekday: "long" }
-);
+const today =
+new Date().toLocaleDateString("en-US",{weekday:"long"});
 
 document.getElementById("dayTitle").textContent = today;
 
@@ -84,110 +370,69 @@ const restDay =
 document.getElementById("restDay");
 
 if(!workouts[today]){
-workoutContainer.style.display = "none";
+workoutContainer.style.display="none";
 restDay.classList.remove("hidden");
 }
 
-let currentExercise = 0;
+let index=0;
 
-function loadExercise(){
+function load(){
 
-const exercise =
-workouts[today][currentExercise];
+const ex=workouts[today][index];
 
-document.getElementById("exerciseName").textContent =
-exercise.name;
+document.getElementById("exerciseName").textContent=ex.name;
+document.getElementById("sets").textContent=ex.sets;
+document.getElementById("reps").textContent=ex.reps;
+document.getElementById("restTime").textContent=ex.rest;
+document.getElementById("tempo").textContent=ex.tempo;
 
-document.getElementById("sets").textContent =
-exercise.sets;
+const list=document.getElementById("instructionsList");
+list.innerHTML="";
 
-document.getElementById("reps").textContent =
-exercise.reps;
-
-document.getElementById("instructions").textContent =
-exercise.instructions;
+ex.instructions.forEach(i=>{
+const li=document.createElement("li");
+li.textContent=i;
+list.appendChild(li);
+});
 }
 
-if(workouts[today]){
-loadExercise();
-}
+if(workouts[today]) load();
 
-let startY = 0;
+let startY=0;
 
-document.addEventListener("touchstart",(e)=>{
-
-startY = e.touches[0].clientY;
-
+document.addEventListener("touchstart",e=>{
+startY=e.touches[0].clientY;
 });
 
-document.addEventListener("touchend",(e)=>{
+document.addEventListener("touchend",e=>{
+let endY=e.changedTouches[0].clientY;
 
-const endY =
-e.changedTouches[0].clientY;
+if(startY-endY>100){
 
-if(startY - endY > 100){
-
-const popup =
-document.getElementById("levelPopup");
-
+const popup=document.getElementById("levelPopup");
 popup.classList.remove("hidden");
 
 setTimeout(()=>{
 
 popup.classList.add("hidden");
 
-currentExercise++;
+index++;
 
-if(currentExercise < workouts[today].length){
-
-loadExercise();
-
+if(index<workouts[today].length){
+load();
 }else{
-
-document.querySelector(".exercise-card").innerHTML = `
-
-<div style="
-height:100%;
-display:flex;
-flex-direction:column;
-justify-content:center;
-align-items:center;
-text-align:center;
-">
-
-<h1 style="
-font-size:56px;
-font-weight:900;
-background:linear-gradient(
-90deg,
-#22c55e,
-#67e8f9,
-#8b5cf6
-);
--webkit-background-clip:text;
--webkit-text-fill-color:transparent;
-">
+document.querySelector(".exercise-card").innerHTML=`
+<div style="height:100%;display:flex;flex-direction:column;justify-content:center;align-items:center;text-align:center;">
+<h1 style="font-size:54px;font-weight:900;background:linear-gradient(90deg,#22c55e,#67e8f9,#8b5cf6);-webkit-background-clip:text;-webkit-text-fill-color:transparent;">
 Workout Complete
 </h1>
-
-<p style="
-margin-top:20px;
-font-size:18px;
-max-width:280px;
-line-height:1.7;
-color:#94a3b8;
-">
-You gained experience today.
-Return tomorrow for your next mission.
+<p style="margin-top:15px;color:#94a3b8;max-width:260px;line-height:1.6;">
+You completed today's mission. Come back tomorrow.
 </p>
-
-</div>
-`;
-
+</div>`;
 }
 
-},1200);
+},1000);
 
 }
-
 });
